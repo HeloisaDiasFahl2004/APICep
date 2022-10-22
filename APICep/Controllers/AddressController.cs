256330f09be8ba07b62c5e47181dc085a70135ac
helofahl@gmail.com <helofahl@gmail.com>
2022-10-22 17:14:06 -0300

    Adicionar arquivos de projeto.

---
+++ APICep/Controllers/AddressController.cs
@@ -0,0 +1,25 @@
+﻿using APICep.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICep.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        public AddressController()
+        {
+            //vazio pois no método getAdress já tem a requisição HTTP
+        }
+        [HttpGet("{cep}")]
+        public ActionResult<string> GetAddress(string cep)
+        {
+            var address = new AddressService().GetAddress(cep);
+
+            if (address == null) return NotFound();
+
+            return Ok(address);
+        }
+    }
+}
---
+++ APICep/Service/AddressService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Net;
+
+namespace APICep.Service
+{
+    public class AddressService
+    {
+        public AddressService()
+        {
+
+        }
+
+        public string GetAddress(string cep) //método utilizado para executar uma requisição web
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + cep + "/json/"); //url
+            request.AllowAutoRedirect = false;
+            HttpWebResponse verificaServidor = (HttpWebResponse)request.GetResponse();
+            Stream stream = verificaServidor.GetResponseStream();
+            if (stream == null) return null;
+            StreamReader answerReader = new StreamReader(stream);
+            string message = answerReader.ReadToEnd();
+            return message;
+
+        }
+
+    }
+}
